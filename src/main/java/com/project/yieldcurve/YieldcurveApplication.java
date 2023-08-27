71cc91711602fb3e0ee76da959326341e537b3fc
oguzhan-yilmazz <ous2ylmz@gmail.com>
2023-08-27 03:23:17 +0300

    Heroku için güncellemeler

--- src/main/java/com/project/yieldcurve/YieldcurveApplication.java
+++ src/main/java/com/project/yieldcurve/YieldcurveApplication.java
@@ -7,10 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 import com.project.yieldcurve.service.YieldCurveService;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class YieldcurveApplication implements CommandLineRunner {
 
     @Autowired
