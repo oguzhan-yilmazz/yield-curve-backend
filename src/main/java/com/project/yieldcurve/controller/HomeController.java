1240c18b1f4e148449b7a2c518f5bf86b08e3501
oguzhan-yilmazz <ous2ylmz@gmail.com>
2023-08-27 03:47:08 +0300

    heroku için diğişiklikler yapıldı

--- src/main/java/com/project/yieldcurve/controller/HomeController.java
+++ src/main/java/com/project/yieldcurve/controller/HomeController.java
@@ -1,13 +1,14 @@
 package com.project.yieldcurve.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello, World!";
+        return "home";
     }
 }
